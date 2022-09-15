b69cf6578604244f061829e8c94734d986149aef
Natalia <natalia-321s@outlook.com>
2022-09-15 00:10:37 -0300

    Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,28 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get("/games", (request, response) => {
+  response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
   return response.json([
-    {
-      "id": 1,
-      "name": 'Ad 1'
-    },
-    {
-      "id": 2,
-      "name": 'Ad 2'
-    },
-    {
-      "id": 3,
-      "name": 'Ad 3'
-    },
-  ])
-  
+    { id: 1, name: "Ad 1", },
+    { id: 2, name: "Ad 2", },
+    { id: 3, name: "Ad 3", },
+    { id: 4, name: "Ad 4", },
+  ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const adId = request.params.id;
+  return response.json([])
 })
 
 app.listen(3333);
